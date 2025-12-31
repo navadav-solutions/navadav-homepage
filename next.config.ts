37cb58a82d6ee697629496093f7389cc2e2f6944
D_Navarro <david.navarro1@dellteam.com>
2025-12-31 10:00:39 -0500

    testing gitpage

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,11 @@
 import type { NextConfig } from "next";
 
-// Configuración del basePath para GitHub Pages
-// En desarrollo (local), no se usa basePath
-// En producción (GitHub Pages), se usa "/navadav" si está configurado
-const basePath = "";
-
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: basePath || undefined,
-  assetPrefix: basePath || undefined,
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
 };
-
-
 
 export default nextConfig;
