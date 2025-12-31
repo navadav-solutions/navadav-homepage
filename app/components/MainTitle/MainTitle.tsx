bf37a80dbe3152473bc758b82a153b68fee45c38
David Navarro <davidnavarro@globant.com>
2025-12-30 21:09:17 -0500

    cambio de textos

--- app/components/MainTitle/MainTitle.tsx
+++ app/components/MainTitle/MainTitle.tsx
@@ -1,30 +1,29 @@
 const MainTitle = () => {
   return (
-    <section className="text-center lg:text-left">
-      <h1
-        className="font-bold text-gray-800 leading-tight
-                     text-3xl sm:text-4xl lg:text-[72px]"
-      >
+    <h1 className="text-[32px] lg:text-[72px] font-[700] mb-10">
+      <span className="text-gray-800 leading-[1.3]">
         Tecnología en evolución <br className="hidden lg:block" />
         para tu{" "}
         <span
-          className="inline-block bg-gradient-to-r
-                     from-[#FAC354] via-[#EF2A2A] to-[#32C3EA]
-                     bg-clip-text text-transparent"
+          className="inline-block"
+          style={{
+            background:
+              "linear-gradient(270deg, #FAC354 -5.84%, #EF2A2A 4.3%, #340551 14.64%, #2C0296 26.14%, #3155E0 40.04%, #32C3EA 56.2%)",
+            WebkitBackgroundClip: "text",
+            WebkitTextFillColor: "transparent",
+            backgroundClip: "text",
+          }}
         >
           negocio
-        </span>
-      </h1>
-
-      <p
-        className="mt-6 text-gray-600
-                    text-base sm:text-lg lg:text-2xl
-                    max-w-xl lg:max-w-3xl mx-auto lg:mx-0"
-      >
-        Diseñamos y mejoramos sistemas empresariales para aumentar productividad
-        y sostener tu crecimiento.
-      </p>
-    </section>
+        </span>{" "}
+      </span>
+      <div className="inline-flex flex-col items-center justify-center">
+        <p className="text-[18px] lg:text-[24px] font-[700] text-gray-500 w-[80%] m-[0px]">
+          Diseñamos y mejoramos sistemas empresariales para aumentar
+          productividad y sostener tu crecimiento.
+        </p>
+      </div>
+    </h1>
   );
 };
 
